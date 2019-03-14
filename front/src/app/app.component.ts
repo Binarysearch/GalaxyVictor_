00714cb10b392361f74f5575d74e48f3f3a87963
Antonio Ruiz <desenlace.net@gmail.com>
2019-03-14 10:44:30 +0100

    Merge old front

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'front';
+export class AppComponent implements OnInit{
+  asideMenuVisible = false;
+
+  constructor(private authService: AuthService) {}
+
+  closeAsideMenu() {
+    this.asideMenuVisible = false;
+  }
+
+  toggleAsideMenu() {
+    this.asideMenuVisible = !this.asideMenuVisible;
+  }
+
+  ngOnInit(): void {
+    this.authService.auth();
+  }
+
 }
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,16 +1,71 @@
+import { HttpInterceptorService } from './services/http-interceptor.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from './app-routing.module';
+import { GalaxyComponent } from './galaxy/galaxy.component';
+import { ColoniesComponent } from './colonies/colonies.component';
+import { FleetsComponent } from './fleets/fleets.component';
+import { PlanetsComponent } from './planets/planets.component';
+import { CommerceComponent } from './commerce/commerce.component';
+import { ResearchComponent } from './research/research.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LandingComponent } from './landing/landing.component';
+import { GalaxiesComponent } from './galaxies/galaxies.component';
+import { StarSystemWindowComponent } from './galaxy/star-system-window/star-system-window.component';
+import { StarDetailsComponent } from './galaxy/star-system-window/star-details/star-details.component';
+import { StarPlanetsComponent } from './galaxy/star-system-window/star-planets/star-planets.component';
+import { StarFleetsComponent } from './galaxy/star-system-window/star-fleets/star-fleets.component';
+import { CreateCivilizationWindowComponent } from './galaxy/create-civilization-window/create-civilization-window.component';
+import { CivilizationsComponent } from './civilizations/civilizations.component';
+import { PlanetWindowComponent } from './galaxy/planet-window/planet-window.component';
+import { PlanetDetailsComponent } from './galaxy/planet-window/planet-details/planet-details.component';
+import { ColonyDetailsComponent } from './galaxy/planet-window/colony-details/colony-details.component';
+import { FleetWindowComponent } from './galaxy/fleet-window/fleet-window.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AsideComponent,
+    FooterComponent,
+    GalaxyComponent,
+    ColoniesComponent,
+    FleetsComponent,
+    PlanetsComponent,
+    CommerceComponent,
+    ResearchComponent,
+    LoginComponent,
+    RegisterComponent,
+    LandingComponent,
+    GalaxiesComponent,
+    StarSystemWindowComponent,
+    StarDetailsComponent,
+    StarPlanetsComponent,
+    StarFleetsComponent,
+    CreateCivilizationWindowComponent,
+    CivilizationsComponent,
+    PlanetWindowComponent,
+    PlanetDetailsComponent,
+    ColonyDetailsComponent,
+    FleetWindowComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: HttpInterceptorService, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
