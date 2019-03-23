950447ba63cac8d64f7188854964870f14532fd2
Antonio Ruiz <desenlace.net@gmail.com>
2019-03-23 09:48:22 +0100

    Creado test para get colonies y colony buildings

---
+++ back/src/main/resources/db/procedures/get_colony_buildings.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION core.get_colony_buildings(colony_ bigint, token_ text)
+ RETURNS json
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$declare
+  result_ json;
+  civilization_id_ bigint;
+begin
+
+  civilization_id_ = core.require_civ(token_);
+
+  -- if civilization does not own the colony throw error
+  if (not exists(select 1 from core.colonies where id=colony_ and civilization=civilization_id_)) then
+    perform core.error(401, 'Colony not yours');
+  end if;
+
+  result_ = (with x as (
+    
+    select id, building_type from core.colony_buildings where colony=colony_
+
+  ) select array_to_json(array_agg(x)) from x);
+
+  return coalesce(result_, '[]')::json;
+end;$function$;
---
+++ back/src/test/java/com/galaxyvictor/civilizations/ColonyDetailsTest.java
@@ -0,0 +1,90 @@
+package com.galaxyvictor.civilizations;
+
+import static com.jayway.jsonpath.JsonPath.read;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+import java.sql.SQLException;
+
+import com.galaxyvictor.BaseTest;
+import com.galaxyvictor.servlet.ApiRequest;
+import com.galaxyvictor.servlet.civilization.CivilizationController;
+import com.galaxyvictor.servlet.civilization.CivilizationsController;
+import com.galaxyvictor.servlet.civilization.ColoniesController;
+import com.galaxyvictor.servlet.civilization.ColonyBuildingsController;
+import com.galaxyvictor.servlet.galaxies.CurrentGalaxyController;
+import com.galaxyvictor.servlet.galaxies.GalaxiesController;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class ColonyDetailsTest extends BaseTest {
+
+    private static int civilizationId;
+    private static int homeworldId;
+    private static String civilizationName = "Psilons";
+
+    @BeforeAll
+    public static void setupTest() throws SQLException {
+
+        //get galaxies
+        ApiRequest getGalaxiesRequest = mock(ApiRequest.class);
+        given(getGalaxiesRequest.getToken()).willReturn(token);
+        String getGalaxiesResponse = new GalaxiesController().getRequest(getGalaxiesRequest);
+        int galaxyId = read(getGalaxiesResponse, "[0].id");
+
+        //set current galaxy
+        ApiRequest currentGalaxyRequest = mock(ApiRequest.class);
+        given(currentGalaxyRequest.getToken()).willReturn(token);
+        given(currentGalaxyRequest.jsonPath("$.id")).willReturn(galaxyId);
+        new CurrentGalaxyController().putRequest(currentGalaxyRequest);
+
+        //create civilization
+        ApiRequest request = mock(ApiRequest.class);
+        given(request.getToken()).willReturn(token);
+        given(request.jsonPath("$.name")).willReturn(civilizationName);
+        given(request.jsonPath("$.homeStarName")).willReturn("Mentar");
+        String result = new CivilizationController().postRequest(request);
+        civilizationId = read(result, "$.id");
+        homeworldId = read(result, "$.homeworld.id");
+        
+    }
+
+    @Test
+    public void testGetColonyDetails() throws SQLException {
+
+
+        ApiRequest coloniesRequest = mock(ApiRequest.class);
+        given(coloniesRequest.getToken()).willReturn(token);
+        String coloniesResponse = new ColoniesController().getRequest(coloniesRequest);
+
+        int colonyId = (int) read(coloniesResponse, "[0].id");
+        assertEquals((int) read(coloniesResponse, "[0].civilization"), civilizationId);
+        assertEquals((int) read(coloniesResponse, "[0].planet"), homeworldId);
+
+        //Colony buildings
+        ApiRequest colonyBuildingsRequest = mock(ApiRequest.class);
+        given(colonyBuildingsRequest.getToken()).willReturn(token);
+        given(colonyBuildingsRequest.jsonPath("$.colony")).willReturn(colonyId);
+        String colonyBuildingsResponse = new ColonyBuildingsController().getRequest(colonyBuildingsRequest);
+        
+        assertNotNull(read(colonyBuildingsResponse, "[0].id"));
+        assertEquals(read(colonyBuildingsResponse, "[0].building_type"), "imperial capital");
+    }
+
+    @Test
+    public void testGetCivilizations() throws SQLException {
+        ApiRequest request = mock(ApiRequest.class);
+
+        given(request.getToken()).willReturn(token);
+
+        String response = new CivilizationsController().getRequest(request);
+
+        assertEquals((int) read(response, "[0].id"), civilizationId);
+        assertEquals(read(response, "[0].name"), civilizationName);
+        assertEquals((int) read(response, "[0].homeworld"), homeworldId);
+        
+    }
+}
