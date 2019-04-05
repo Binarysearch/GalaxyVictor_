b2aa9575ed051d9b266d3337cccb2bcc8397a4f9
Antonio Ruiz <desenlace.net@gmail.com>
2019-04-05 13:28:48 +0200

    Comentarios en set_research_order

--- back/src/main/resources/db/tests/proc/set_research_order.sql
+++ back/src/main/resources/db/tests/proc/set_research_order.sql
@@ -13,27 +13,25 @@ begin
   stellar_government_ = id from core.stellar_governments where star_system=2 and civilization=1;
 
   result_ = core.set_research_order(2, 'fusion', 12345, 'token');
+
+  -- Test response format
   perform test.research_order_dto(result_ #> '{messageOrders, 0, payload}');
   perform test.message_order_dto(result_ #> '{messageOrders, 0}');
   perform test.asinc_task_order_dto(result_ #> '{asincTasks, 0}');
 
+  -- Test correct message order created
   assert (result_ #>> '{messageOrders, 0, civilizations, 0}')::numeric = 1, 'Message sent to other civilization';
   assert (result_ #>> '{messageOrders, 0, civilizations, 1}') is null, 'Message sent to other civilization';
-  
   assert (result_ #>> '{messageOrders, 0, type}') = 'ResearchOrder', 'Message is not ResearchOrder';
-  
   assert (result_ #>> '{messageOrders, 1}') is null, 'Sent too many messages';
 
+  -- Test correct asinc task created
   assert (result_ #>> '{asincTasks, 0, id}') = (result_ #>> '{asincTaskCancelOrders, 0}'), 'Cancel order id and new order id does not match';
-
-  assert (result_ #>> '{asincTasks, 0, id}')::numeric = stellar_government_, 'Incorrect stellar_government';
-
   assert (result_ #>> '{asincTasks, 0, id}')::numeric = stellar_government_, 'Incorrect stellar_government';
   assert (result_ #>> '{asincTasks, 0, procedureName}') = 'core.finish_research_order', 'Incorrect callback name';
-  
   assert (result_ #>> '{asincTasks, 1}') is null, 'Created too many asinc tasks';
 
-
+  -- Test correct technology and star system
   assert (exists(select 1 from core.research_orders where stellar_government=stellar_government_ and technology='fusion')), 'Research Order not saved correctly';
 
 end;$function$;
