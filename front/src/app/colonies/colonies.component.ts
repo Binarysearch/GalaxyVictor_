39e62ee47d888a5b9651b69f7d363065f11df952
Antonio Ruiz <desenlace.net@gmail.com>
2019-04-18 10:14:33 +0200

    Lista de colonias con boton de construcciones

--- front/src/app/colonies/colonies.component.ts
+++ front/src/app/colonies/colonies.component.ts
@@ -19,7 +19,12 @@ export class ColoniesComponent implements OnInit {
   constructor(private store: Store, public ts: TextService, private galaxyMap: GalaxyMap, private router: Router) { }
 
   ngOnInit() {
-
+    const state = localStorage.getItem('colonies-window');
+    if (state) {
+      const stateObj = JSON.parse(state);
+      this.orderBy = stateObj.orderBy;
+      this.orderDir = stateObj.orderDir;
+    }
   }
 
   get colonies(): Colony[] {
@@ -30,12 +35,38 @@ export class ColoniesComponent implements OnInit {
         this._colonies.push(c);
       });
 
-      this._colonies.sort((c1, c2) => {
-        return this.orderDir * (c1[this.orderBy] - c2[this.orderBy]);
-      });
+      if (this.orderBy === 'construction') {
+        this._colonies.sort((c1, c2) => {
+          const cc1 = c1.buildingOrder ? 1 : c1.shipOrder ? 2 : 0;
+          const cc2 = c2.buildingOrder ? 1 : c2.shipOrder ? 2 : 0;
+          return this.orderDir * (cc1 - cc2);
+        });
+      } else if (this.orderBy === 'name') {
+        this._colonies.sort((c1, c2) => {
+          return this.orderDir * ('' + c1.planet.name).localeCompare(c2.planet.name);
+        });
+      } else {
+        this._colonies.sort((c1, c2) => {
+          return this.orderDir * (c1[this.orderBy] - c2[this.orderBy]);
+        });
+      }
     }
 
     return this._colonies;
+  }
+
+  public getConstruction(colony: Colony): string {
+    if (colony.buildingOrder) {
+      return colony.buildingOrderName;
+    } else if (colony.shipOrder) {
+      return colony.shipOrderName;
+    } else {
+      return this.ts.strings.noBuildingOrder;
+    }
+  }
+
+  public changeBuildingOrderClicked(colony: Colony) {
+
   }
 
   public sortBy(fieldName: string) {
@@ -45,10 +76,11 @@ export class ColoniesComponent implements OnInit {
       this.orderBy = fieldName;
     }
     this._colonies = [];
+    localStorage.setItem('colonies-window', JSON.stringify({orderBy: this.orderBy, orderDir: this.orderDir}));
   }
 
-  public clickInColony(id: number) {
-    this.galaxyMap.selectAndFocus(id);
+  public clickInColony(colony: Colony) {
+    this.galaxyMap.selectAndFocus(colony.planetId);
     this.router.navigate(['galaxy']);
   }
 }
