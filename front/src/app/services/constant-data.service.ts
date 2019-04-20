5146edb75b72b9543abc470993c83049e820409f
Antonio Ruiz <desenlace.net@gmail.com>
2019-04-20 19:55:51 +0200

    Cache de datos contantes en local storage

--- front/src/app/services/constant-data.service.ts
+++ front/src/app/services/constant-data.service.ts
@@ -30,71 +30,80 @@ export class ConstantDataService {
   private constantDataUrl = this.host + '/api/constant-data';
 
   constructor(private http: HttpClient, private store: Store, private authService: AuthService) {
-   this.authService.getCurrentSession().subscribe((session: SessionDTO) => {
-     if (session) {
-       this.loadConstantData();
-     }
-   });
+
+    const constantDataString = localStorage.getItem('constant-data');
+    if (constantDataString) {
+      this.formatConstantData(JSON.parse(constantDataString) as ConstantDataDTO);
+    } else {
+      this.authService.getCurrentSession().subscribe((session: SessionDTO) => {
+        if (session) {
+          this.loadConstantData();
+        }
+      });
+    }
  }
 
   private loadConstantData() {
     this.http.get<ConstantDataDTO>(this.constantDataUrl)
     .subscribe((data: ConstantDataDTO) => {
-
-      // technologies
-      data.technologies.forEach(t => {
-        const technology = new Technology(t);
-        if (t.prerequisites) {
-          t.prerequisites.forEach(id => {
-            const prerequisite = this.store.getTechnology(id);
-            technology.prerequisites.push(prerequisite);
-          });
-        }
-        this.store.addTechnology(technology);
-      });
-
-      // resource types
-      data.resourceTypes.forEach(c => {
-        const resourceType = new ResourceType(c);
-        this.store.addResourceType(resourceType);
-      });
-
-      // colony building capability types
-      data.colonyBuildingCapabilityTypes.forEach(c => {
-        const capabilityType = new ColonyBuildingCapabilityType(c);
-        this.store.addColonyBuildingCapabilityType(capabilityType);
-      });
-
-      // colony building types
-      data.colonyBuildingTypes.forEach(c => {
-        const buildingType = new ColonyBuildingType(c);
-        if (c.resources) {
-          c.resources.forEach(r => {
-            buildingType.resources.push({resourceType: this.store.getResourceType(r.type), quantity: r.quantity});
-          });
-        }
-        if (c.costs) {
-          c.costs.forEach(r => {
-            buildingType.costs.push({resourceType: this.store.getResourceType(r.type), quantity: r.quantity});
-          });
-        }
-        if (c.capabilities) {
-          c.capabilities.forEach(r => {
-            buildingType.capabilities.push({type: this.store.getColonyBuildingCapabilityType(r.type)});
-          });
-        }
-        if (c.prerequisites) {
-          c.prerequisites.forEach(id => {
-            const prerequisite = this.store.getTechnology(id);
-            buildingType.prerequisites.push(prerequisite);
-          });
-        }
-        this.store.addColonyBuildingType(buildingType);
-      });
-
+      localStorage.setItem('constant-data', JSON.stringify(data));
+      this.formatConstantData(data);
       console.log(this.store);
     }, (error: any) => {
       console.log(error);
+    });
+  }
+
+  formatConstantData(data: ConstantDataDTO) {
+    // technologies
+    data.technologies.forEach(t => {
+      const technology = new Technology(t);
+      if (t.prerequisites) {
+        t.prerequisites.forEach(id => {
+          const prerequisite = this.store.getTechnology(id);
+          technology.prerequisites.push(prerequisite);
+        });
+      }
+      this.store.addTechnology(technology);
+    });
+
+    // resource types
+    data.resourceTypes.forEach(c => {
+      const resourceType = new ResourceType(c);
+      this.store.addResourceType(resourceType);
+    });
+
+    // colony building capability types
+    data.colonyBuildingCapabilityTypes.forEach(c => {
+      const capabilityType = new ColonyBuildingCapabilityType(c);
+      this.store.addColonyBuildingCapabilityType(capabilityType);
+    });
+
+    // colony building types
+    data.colonyBuildingTypes.forEach(c => {
+      const buildingType = new ColonyBuildingType(c);
+      if (c.resources) {
+        c.resources.forEach(r => {
+          buildingType.resources.push({resourceType: this.store.getResourceType(r.type), quantity: r.quantity});
+        });
+      }
+      if (c.costs) {
+        c.costs.forEach(r => {
+          buildingType.costs.push({resourceType: this.store.getResourceType(r.type), quantity: r.quantity});
+        });
+      }
+      if (c.capabilities) {
+        c.capabilities.forEach(r => {
+          buildingType.capabilities.push({type: this.store.getColonyBuildingCapabilityType(r.type)});
+        });
+      }
+      if (c.prerequisites) {
+        c.prerequisites.forEach(id => {
+          const prerequisite = this.store.getTechnology(id);
+          buildingType.prerequisites.push(prerequisite);
+        });
+      }
+      this.store.addColonyBuildingType(buildingType);
     });
   }
 }
